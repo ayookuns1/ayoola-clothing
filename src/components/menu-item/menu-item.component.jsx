2162e611df571d31b628e9cae7bab1835240069d
unknown <ayookuns830@gmail.com>
2023-07-18 15:21:58 +0100

    routing added

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,25 +1,23 @@
-import React from "react";
-import './menu-item.styles.scss'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+import './menu-item.styles.scss';
 
-
-const MenuItem = ({title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        <div className="background-image"
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  return (
+    <Link to={linkUrl} className={`${size} menu-item`}>
+      <div
+        className="background-image"
         style={{
-            backgroundImage: `url(${imageUrl})`
-        }} 
-        />
-            <div className="content">
-                <h1 className="title">
-                    {title.toUpperCase()}
-                </h1>
-                <span className="subtitle">
-                     SHOP NOW
-                </span>
-            </div>
-          </div>
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </Link>
+  );
+};
 
-);
-
-export default MenuItem
+export default MenuItem;
