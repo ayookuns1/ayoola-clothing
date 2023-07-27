c6b33d5030631784521a31dde6ee9966d6b3c9f0
Ayoola Okunlola <ayookuns830@gmail.com>
2023-07-27 12:03:25 +0100

    testing new workspace

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
       <Route exact path='/' element={<HomePage />} />
       <Route path='/shop' element={<ShopPage />} />
+      <Route path='/signin' element={<SignInAndSignUpPage/>} />
       </Routes>
     </div>
   )
